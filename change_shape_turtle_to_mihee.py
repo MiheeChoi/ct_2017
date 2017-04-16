b79bb7ab1c59a3c43f5392d44b7c15957ca6f713
MiheeChoi <vip0020@naver.com>
2017-04-17 00:40:48 +0900

    Add files via upload

---
+++ change_shape_turtle_to_mihee.py
@@ -0,0 +1,9 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> wn=turtle.Screen()
+>>> t1=turtle.Turtle()
+>>> t1.shape("turtle")
+>>> wn.addshape("C:\\Users\\user\\Pictures\\사진\\mihee_pic1.gif")
+>>> t1.shape("C:\\Users\\user\\Pictures\\사진\\mihee_pic1.gif")
+>>> 
