d0981a5cdffae6f87270a8e52df06946492c4982
MiheeChoi <vip0020@naver.com>
2017-05-16 00:23:57 +0900

    Add files via upload

---
+++ windtoy_fast.py
@@ -0,0 +1,30 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+oldpos=t1.pos()
+size=100
+
+def giyuk(size):
+    t1.fd(size)
+    t1.rt(90)
+    t1.fd(size)
+    t1.penup()
+    t1.setpos(oldpos)
+    oldhead=t1.heading()
+    t1.setheading(oldhead+45)
+    t1.pendown()
+
+
+    
+
+giyuk(size)
+giyuk(size)
+giyuk(size)
+giyuk(size)
+giyuk(size)
+giyuk(size)
+giyuk(size)
+giyuk(size)
+
+wn.exitonclick()
