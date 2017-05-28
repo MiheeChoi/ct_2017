d9e1eac360f6ff6c7a158042cee801e2c4815dba
MiheeChoi <vip0020@naver.com>
2017-05-28 20:44:24 +0900

    Add files via upload

---
+++ giyuk_nien_miem_AT.py
@@ -0,0 +1,40 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def giyuk(size):
+    t1.fd(size)
+    t1.right(90)
+    t1.fd(size)
+
+def nien(size):
+    t1.fd(size)
+    t1.left(90)
+    t1.fd(size)
+
+def giyukAT(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    giyuk(size)
+giyukAT(-100,100,100)
+
+def nienAT(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    nien(size)
+nienAT(-100,0,100)
+
+def miemAT(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    giyuk(size)
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    nien(size)
+miemAT(-100,-150,100)
+
+wn.exitonclick()
